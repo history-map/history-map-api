1837c67d1e41c22df7f2d1123de609c75e472424
Yang Li <idealhack@gmail.com>
2015-12-27 04:04:13 +0800

    add nearby buildings query

--- common/models/building.js
+++ common/models/building.js
@@ -1,3 +1,42 @@
-module.exports = function(Building) {
+var loopback = require('loopback');
 
+module.exports = function(Building) {
+  Building.nearby = function(lat, lng, cb) {
+    var here = new loopback.GeoPoint({
+      lat: lat,
+      lng: lng
+    });
+
+    Building.find({
+      where: {
+        location: {
+          near: here
+        }
+      },
+      limit: 10
+    }, function(err, nearbyBuildings) {
+      console.info(nearbyBuildings);
+      cb(null, nearbyBuildings);
+    });
+  };
+
+  Building.remoteMethod(
+    'nearby', {
+      http: {
+        path: '/nearby',
+        verb: 'get'
+      },
+      accepts: [{
+        arg: 'lat',
+        type: 'string'
+      }, {
+        arg: 'lng',
+        type: 'string'
+      }],
+      returns: {
+        arg: 'buildings',
+        type: 'array'
+      }
+    }
+  );
 };
